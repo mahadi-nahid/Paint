2bf4bf900a73db12618046d140ed0b4e807b901f
Md. Mahadi Hasan Nahid <nahid@student.sust.edu>
2014-11-02 23:18:42 -0800

    Created Project-Paint

---
+++ Paint/src/paint/Paint.java
@@ -0,0 +1,17 @@
+
+package paint;
+
+/**
+ *
+ * @author MdMahadiHasan
+ */
+public class Paint {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
